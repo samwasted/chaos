41602f31c3d927ae80e4154e4f5dda7a76206660
samwasted <samarpancodes@gmail.com>
2025-09-13 13:22:26 +0530

    bochs setup, needs bug fix

--- src/kernel/main.asm
+++ src/kernel/main.asm
@@ -1,13 +1,10 @@
 org 0x7C00
 bits 16
 
-
 %define ENDL 0x0D, 0x0A
-
 
 start:
     jmp main
-
 
 ;
 ; Prints a string to the screen
@@ -15,18 +12,17 @@ start:
 ;   - ds:si points to string
 ;
 puts:
-    ; save registers we will modify
     push si
     push ax
     push bx
 
 .loop:
-    lodsb               ; loads next character in al
-    or al, al           ; verify if next character is null?
+    lodsb               ; AL = [DS:SI], SI++
+    or al, al
     jz .done
 
-    mov ah, 0x0E        ; call bios interrupt
-    mov bh, 0           ; set page number to 0
+    mov ah, 0x0E        ; teletype BIOS
+    mov bh, 0
     int 0x10
 
     jmp .loop
@@ -34,33 +30,33 @@ puts:
 .done:
     pop bx
     pop ax
-    pop si    
+    pop si
     ret
-    
 
 main:
     ; setup data segments
-    mov ax, 0           ; can't set ds/es directly
+    xor ax, ax
     mov ds, ax
     mov es, ax
-    
-    ; setup stack
+
+    ; setup stack (leave some room so pushes don't clobber code)
     mov ss, ax
-    mov sp, 0x7C00      ; stack grows downwards from where we are loaded in memory
+    mov sp, 0x7A00      ; safer: stack at 0x0000:0x7A00 (leaves 0x200 bytes)
 
     ; print hello world message
     mov si, msg_hello
     call puts
 
+    cli
     hlt
 
-.halt
-    jmp .halt
-
-
+.halt:
+    cli
+.halt_loop:
+    hlt
+    jmp .halt_loop
 
 msg_hello: db 'Hello world!', ENDL, 0
-
 
 times 510-($-$$) db 0
 dw 0AA55h
