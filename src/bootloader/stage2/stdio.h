6fc392ebd3d3592ae3bc9f29b85d4e2a431c4a26
samwasted <samarpancodes@gmail.com>
2025-09-14 18:48:04 +0530

    implement printf

--- src/bootloader/stage2/stdio.h
+++ src/bootloader/stage2/stdio.h
@@ -1,16 +1,10 @@
 #pragma once
 
-typedef signed char int8_t;
-typedef unsigned char uint8_t;
-typedef signed short int16_t;
-typedef unsigned short uint16_t;
-typedef signed long int int32_t;
-typedef unsigned long int uint32_t;
-typedef signed long long int int64_t;
-typedef unsigned long long int uint64_t;
+#ifndef far
+#define far
+#endif
 
-typedef uint8_t bool;
-
-#define false 0
-#define true 1
-
+void putc(char c);
+void puts(const char* str);
+void puts_f(const char far* str);
+void _cdecl printf(const char* fmt, ...); // ignore this error squiggle, we are overriding our own printf
--- src/bootloader/stage2/x86.h
+++ src/bootloader/stage2/x86.h
@@ -1,4 +1,4 @@
-#pragma once
+#pragma once //tells compare to include header only once, even if it tries to include multiple times
 #include "stdint.h"
 
 
@@ -7,4 +7,5 @@
 #endif
 
 
+void _cdecl x86_div64_32(uint64_t dividend, uint32_t divisor, uint64_t* quotientOut, uint32_t* remainderOut);
 void _cdecl x86_Video_WriteCharTeletype(char c, uint8_t page);
